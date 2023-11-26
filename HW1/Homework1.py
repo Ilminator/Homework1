4149a8abb9c254d0afafa1cd6c1c3815342d39cf
Ilya <146206338+Ilminator@users.noreply.github.com>
2023-11-26 20:19:41 +0300

    Update Homework1.py

--- HW1/Homework1.py
+++ HW1/Homework1.py
@@ -5,14 +5,3 @@ for i in range(5):
     oval (90, y, 120, 120 )
     y = y + step
 saveImage("HW1.pdf")
-
-y = 10
-x = 10
-step = 160
-for i in range(5):
-    for t in range(6):
-        oval (x, y, 120, 120 )
-        x = x + step
-    y = y + step
-    x = 10
-saveImage("HW2.pdf")
